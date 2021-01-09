9a4564adc6737213e2d996e5ec1862bef67db2af
yasgo <yasinburakkalkan@gmail.com>
2021-01-09 12:39:40 +0300

    Min char attr development.

--- src/script/common-validation/standart-validation.js
+++ src/script/common-validation/standart-validation.js
@@ -1,46 +1,48 @@
 import { CustomValidation } from "../custom-validation";
 
 const isElementFull = (item) => {
-    return item.val() === "" || item.val() === null;
+  const minChar = item.closest(".supreme-validate-element").attr("min");
+
+  return item.val() === "" || item.val() === null || minChar && item.val().length < minChar;
 };
 
 export default function standartValidate(collection) {
 
-    const errorResponse = collection.filter((item) => {
-        const hasCustomValidation = $(item).parent().attr("custom-validation") !== undefined;
-        const customValidationName = $(item).parent().attr("custom-validation");
+  const errorResponse = collection.filter((item) => {
+    const hasCustomValidation = $(item).parent().attr("custom-validation") !== undefined;
+    const customValidationName = $(item).parent().attr("custom-validation");
 
-        if (hasCustomValidation) {
-            const value = $(item).val();
-            const validationFunc = CustomValidation[customValidationName];
-            const control = validationFunc(value);
+    if (hasCustomValidation) {
+      const value = $(item).val();
+      const validationFunc = CustomValidation[customValidationName];
+      const control = validationFunc(value);
 
-            if (!control) {
-                return item;
-            }
-        } else {
-            if (isElementFull($(item))) {
-                return item;
-            }
-        }
-    });
+      if (!control) {
+        return item;
+      }
+    } else {
+      if (isElementFull($(item))) {
+        return item;
+      }
+    }
+  });
 
-    const successResponse = collection.filter((item) => {
-        const hasCustomValidation = item.parentNode.hasAttribute("custom-validation");
-        const customValidationName = $(item).parent().attr("custom-validation");
+  const successResponse = collection.filter((item) => {
+    const hasCustomValidation = item.parentNode.hasAttribute("custom-validation");
+    const customValidationName = $(item).parent().attr("custom-validation");
 
-        if (hasCustomValidation) {
-            if (CustomValidation[customValidationName]($(item).val())) {
-                return item;
-            }
-        } else {
-            if (!isElementFull($(item)))
-                return item;
-        }
-    });
+    if (hasCustomValidation) {
+      if (CustomValidation[customValidationName]($(item).val())) {
+        return item;
+      }
+    } else {
+      if (!isElementFull($(item)))
+        return item;
+    }
+  });
 
-    return {
-        errorElements: errorResponse,
-        successElements: successResponse
-    };
+  return {
+    errorElements: errorResponse,
+    successElements: successResponse
+  };
 }
